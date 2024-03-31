3643c3d182046d12ca567f458aa0fbac13fcc5b9
Nicholas Wang <clone.of.snake@gmail.com>
2024-03-31 21:02:29 +0000

    Create minter_role in MyToken. Grant role and mint in script.

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -3,10 +3,18 @@
 pragma solidity ^0.8.20;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract MyToken is ERC20 {
+contract MyToken is ERC20, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
     constructor() ERC20("MyToken", "MTK") {
-        _mint(msg.sender, 1000 * 10 ** decimals());
-        // _mint(msg.sender, initialSupply);
+        _mint(msg.sender, 10 * 10 ** decimals());
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    // Only address with MINTER_ROLE can call mint function: (onlyRole or onlyOwner is from OpenZeppelin AccessControl)
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
     }
 }
--- scripts/Tokens.ts
+++ scripts/Tokens.ts
@@ -6,8 +6,35 @@ async function main() {
     const [deployer, account1, account2] = await viem.getWalletClients();
     const tokenContract = await viem.deployContract("MyToken");
     console.log(`Contract deployed at ${tokenContract.address}`);
+
     const totalSupply = await tokenContract.read.totalSupply();
     console.log({ totalSupply });
+
+    // Fetch the role code
+    const code = await tokenContract.read.MINTER_ROLE();
+    
+    // Grant role to account2
+    const roleTx = await tokenContract.write.grantRole([
+        code,
+        account2.account.address,
+    ]);
+    await publicClient.waitForTransactionReceipt({ hash: roleTx });
+    
+    // Mint Transaction. Switch between account2 and deployer and see
+    const mintTx = await tokenContract.write.mint(
+      [deployer.account.address, parseEther("10")],
+      { account: account2.account }
+    );
+    await publicClient.waitForTransactionReceipt({ hash: mintTx });
+
+    // Fetch token data with Promise.all()
+    const [name, symbol, decimals, totalSupplyAfter] = await Promise.all([
+      tokenContract.read.name(),
+      tokenContract.read.symbol(),
+      tokenContract.read.decimals(),
+      tokenContract.read.totalSupply(),
+    ]);
+    console.log({ name, symbol, decimals, totalSupplyAfter });
 }
 
 main().catch((err) => {
