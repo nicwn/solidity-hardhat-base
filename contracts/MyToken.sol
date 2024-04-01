22268b57701ef7dff57a8da1701eda8db841f66e
Nicholas Wang <clone.of.snake@gmail.com>
2024-04-01 08:40:44 +0000

    Transfer tokens. End of class 9.

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -6,6 +6,7 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract MyToken is ERC20, AccessControl {
+    // Create a new "MINTER_ROLE" as an example
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
     constructor() ERC20("MyToken", "MTK") {
--- scripts/Tokens.ts
+++ scripts/Tokens.ts
@@ -1,6 +1,7 @@
 import { viem } from "hardhat";
-import { parseEther } from "viem";
+import { parseEther, formatEther } from "viem";
 
+// Lesson 9 - https://github.com/Encode-Club-Solidity-Bootcamp/Lesson-09
 async function main() {
     const publicClient = await viem.getPublicClient();
     const [deployer, account1, account2] = await viem.getWalletClients();
@@ -10,8 +11,9 @@ async function main() {
     const totalSupply = await tokenContract.read.totalSupply();
     console.log({ totalSupply });
 
-    // Fetch the role code
+    // Fetch the MINTER role code
     const code = await tokenContract.read.MINTER_ROLE();
+    console.log('This is the code for MINTER_ROLE:', { code });
     
     // Grant role to account2
     const roleTx = await tokenContract.write.grantRole([
@@ -22,12 +24,12 @@ async function main() {
     
     // Mint Transaction. Switch between account2 and deployer and see
     const mintTx = await tokenContract.write.mint(
-      [deployer.account.address, parseEther("10")],
-      { account: account2.account }
+      [account2.account.address, parseEther("10")], // Mint to account2, 10 ETH
+      { account: account2.account } // Called mintTx with this account (pay gas), only account2 works.
     );
     await publicClient.waitForTransactionReceipt({ hash: mintTx });
 
-    // Fetch token data with Promise.all()
+    // Fetch token data with Promise.all() - total supply should increase by 10
     const [name, symbol, decimals, totalSupplyAfter] = await Promise.all([
       tokenContract.read.name(),
       tokenContract.read.symbol(),
@@ -35,6 +37,21 @@ async function main() {
       tokenContract.read.totalSupply(),
     ]);
     console.log({ name, symbol, decimals, totalSupplyAfter });
+
+    // Sending a transaction
+    const tx = await tokenContract.write.transfer([
+      account1.account.address,
+      parseEther("2"),
+    ]);
+    await publicClient.waitForTransactionReceipt({ hash: tx });
+
+    // Read the balance of all 3 accounts
+    const myBalance = await tokenContract.read.balanceOf([deployer.account.address]);
+    console.log(`My Balance is ${formatEther(myBalance)} ${symbol}`);
+    const acc1Balance = await tokenContract.read.balanceOf([account1.account.address]);
+    console.log(`Account1 Balance is ${formatEther(acc1Balance)} ${symbol}`);
+    const acc2Balance = await tokenContract.read.balanceOf([account2.account.address]);
+    console.log(`Account2 Balance is ${formatEther(acc2Balance)} ${symbol}`);
 }
 
 main().catch((err) => {
